dd0fdcf28da7677c2828381e091c705c28a08ba1
William Grohe <william.grohe@gmail.com>
2022-05-14 00:58:34 -0300

    Update inicial page

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,13 @@
 import { Widget } from "./components/Widget";
 
 export function App() {
- return <Widget />
+ return (
+ <>
+  <div className="flex flex-col items-center justify-center h-screen">
+    <h1 className="flex justify-center text-9xl font-bold text-zinc-200">FeedWid</h1>
+    <span className="text-4xl">Feedback widget</span>
+  </div>
+  <Widget />
+ </>
+ )
 }
